7ece5341fdf98b825b65c0b251ad5ce0d1cb6fe0
Brage <bragewiseth@icloud.com>
2024-07-11 14:43:07 +0200

    w

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,69 +9,68 @@ return {
             "nvim-telescope/telescope-file-browser.nvim",
         },
         cmd = "Telescope",
-        opts = {
-            defaults = {
-                vimgrep_arguments = {
-                    "rg",
-                    "-L",
-                    "--color=never",
-                    "--no-heading",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                },
-                prompt_prefix = "🔍 ",
-                selection_caret = "  ",
-                entry_prefix = "  ",
-                initial_mode = "normal",
-                selection_strategy = "reset",
-                sorting_strategy = "descending",
-                layout_strategy = "flex",
-                file_ignore_patterns = { "node_modules" },
-                path_display = { "truncate" },
-                winblend = 0,
-                border = {},
-                borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-                color_devicons = true,
-                set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-                mappings = {
-                    n = { ["q"] = require("telescope.actions").close },
-                },
-            },
-
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    sort_lastused = true,
-                    case_mode = "smart_case",
-                },
-                file_browser = {
-                    theme = "ivy",
-                    -- disables netrw and use telescope-file-browser in its place
-                    hijack_netrw = true,
-                    -- mappings = {
-                    --     ["i"] = {
-                    --         -- your custom insert mode mappings
-                    --     },
-                    --     ["n"] = {
-                    --         -- your custom normal mode mappings
-                    --     },
-                    -- },
-                },
-            },
-            pickers = {
-                buffers = {
-                    ignore_current_buffer = false,
-                    sort_lastused = true,
-                }
-            },
-        },
-        config = function(_, opts)
+        config = function(_)
             local telescope = require("telescope")
-            telescope.setup(opts)
+            local fb_actions = require("telescope").extensions.file_browser.actions
+            local actions = require("telescope.actions")
+            telescope.setup({
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "-L",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                    },
+                    prompt_prefix = "🔍 ",
+                    selection_caret = "  ",
+                    entry_prefix = "  ",
+                    initial_mode = "normal",
+                    selection_strategy = "reset",
+                    sorting_strategy = "descending",
+                    layout_strategy = "flex",
+                    file_ignore_patterns = { "node_modules" },
+                    path_display = { "truncate" },
+                    winblend = 0,
+                    border = {},
+                    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                    color_devicons = true,
+                    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+                    mappings = {
+                        n = { ["q"] = actions.close },
+                    },
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        sort_lastused = true,
+                        case_mode = "smart_case",
+                    },
+                    file_browser = {
+                        theme = "ivy",
+                        -- disables netrw and use telescope-file-browser in its place
+                        hijack_netrw = true,
+                        mappings = {
+                            ["n"] = {
+                                ["l"] = actions.select_default,
+                                ["h"] = fb_actions.goto_parent_dir,
+                                ["H"] = fb_actions.toggle_hidden,
+                            },
+                        },
+                    },
+                },
+                pickers = {
+                    buffers = {
+                        ignore_current_buffer = false,
+                        sort_lastused = true,
+                    }
+                },
+            })
             telescope.load_extension("file_browser")
             telescope.load_extension("fzf")
         end,
